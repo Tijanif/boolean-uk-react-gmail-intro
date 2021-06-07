a1ea9a0c36063e6854696ed3bcc46afbd8058671
Tijani Fulani <Fulani07@gmail.com>
2021-06-07 17:09:41 +0100

    moved css header to its own file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import './App.css'
-
-import flaticonWelcomeImage from './assets/images/flaticon-welcome-image.png'
-import backArrow from './assets/icons/back-arrow.png'
-import downloadButton from './assets/icons/download-button.png'
-import rateStarButton from './assets/icons/rate-star-button.png'
-import rubbishButton from './assets/icons/rubbish-bin-delete-button.png'
 import Header from './components/Header/Header'
 import LeftNav from './components/LeftNav/LeftNav'
 import MainContent from './components/Main/MainContent'
